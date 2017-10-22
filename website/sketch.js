b23c6f39e0e1afeb1f7c7678e7ab008e1832a1f0
Chris <c.lamothe25@gmail.com>
2017-10-22 16:54:15 -0400

    finished 8.6

---
+++ website/sketch.js
@@ -0,0 +1,52 @@
+
+
+
+function setup() {
+  // put setup code here
+  createCanvas(400, 400);
+
+  drawData();
+  console.log('running');
+
+  var button = select('#submit');
+  button.mousePressed(submitWord);
+}
+
+function drawData() {
+  loadJSON('/all', gotData);
+}
+
+function submitWord() {
+  var word = select('#word').value();
+  var score = select('#score').value();
+  console.log(word, score);
+
+  loadJSON('add/' + word + '/' + score, finished);
+
+  function finished(data) {
+    console.log(data);
+    drawData();
+
+  }
+
+}
+
+function gotData(data) {
+    background(51);
+    console.log(data);
+    var keys = Object.keys(data);
+    for (var i = 0; i < keys.length; i++) {
+      var word = keys[i];
+      var score = data[word];
+      var x = random(width);
+      var y = random(height);
+      fill(255);
+      textSize(16);
+      text(word, x, y);
+    }
+}
+
+// function draw() {
+//
+//
+// }
